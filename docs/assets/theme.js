b2409ee48f3d168c3c9cb9b2b62e972cb0590617
Jordi <sroot079@gmail.com>
2023-03-20 20:57:00 -0400

    Update theme.js

--- docs/assets/theme.js
+++ docs/assets/theme.js
@@ -1,6 +1,6 @@
 const STORAGE_KEY = "theme";
 const THEME_ATTR  = "data-theme";
-const QUERY_KEY   = "(prefers-color-scheme: dark)";
+const QUERY_KEY   = "(prefers-color-scheme: light)";
 
 const themes = {
   LIGHT: "light",
